d78d174a57b6d4ba2a2e324ff1fb825f8e684886
CML1986 <carlos.m.lopez86@gmail.com>
2020-01-13 14:06:26 -0300

    adding steps to recreate definitions and End user actions

---
+++ src/test/java/org/carlos/alitest/steps/DefinitionSteps.java
@@ -0,0 +1,46 @@
+package org.carlos.alitest.steps;
+
+import net.thucydides.core.annotations.Steps;
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Named;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
+import org.carlos.alitest.steps.serenity.EndUserSteps;
+
+public class DefinitionSteps {
+
+    @Steps
+    EndUserSteps endUser;
+
+    @Given("the user is on the AliExpress home page")
+    public void givenTheUserIsOnTheAliExpressHomePage() {
+        endUser.is_the_home_page();
+    }
+
+    @When("the user looks up for the word '$word'")
+    public void whenTheUserLooksUpTheWord(String word) {
+        endUser.looks_for(word);
+    }
+
+    @When("the user navigates to <pageN>º page number")
+    public void whenTheUserNavigatesToPageNumber(@Named("pageN") String pageN) {
+        endUser.navigates_to_page(pageN);
+    }
+
+    @When("the user search for the <resultNumber> result")
+    public void whenTheUserSearchForResultItemNumber(@Named("resultNumber") String resultNumber ) {
+        endUser.lookup_resultNumber(resultNumber);
+    }
+
+    @Then("the user asserts that at least '$definition' item is able to buy")
+    public void thenTheyShouldSeeADefinitionContainingTheWords(String definition) {
+        endUser.should_see_definition(definition);
+    }
+
+
+//    And the user navigates to <page> number
+//    And the user search for the <resultNumber> result
+
+
+}
---
+++ src/test/java/org/carlos/alitest/steps/serenity/EndUserSteps.java
@@ -0,0 +1,51 @@
+package org.carlos.alitest.steps.serenity;
+
+import org.carlos.alitest.pages.BasePage;
+import net.thucydides.core.annotations.Step;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasItem;
+
+public class EndUserSteps {
+
+    BasePage aliExpressPage;
+
+    @Step
+    public void enters(String keyword) {
+        aliExpressPage.enter_keywords(keyword);
+    }
+
+    @Step
+    public void starts_search() {
+        aliExpressPage.lookup_terms();
+    }
+
+    @Step
+    public void navigates_to_page(String pageN) {
+        aliExpressPage.lookup_pageNumber(pageN);
+    }
+
+    @Step
+    public void lookup_resultNumber(String resultNumber) {
+        aliExpressPage.lookup_resultNumber(resultNumber);
+    }
+
+    @Step
+    public void should_see_definition(String definition) {
+        if (definition != "0") {
+        assertThat(aliExpressPage.getDefinitions(), hasItem(containsString(definition)));
+        }
+    }
+
+    @Step
+    public void is_the_home_page() {
+        aliExpressPage.open();
+    }
+
+    @Step
+    public void looks_for(String word) {
+        enters(word);
+        starts_search();
+    }
+}
